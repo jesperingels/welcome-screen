801b34ef7257768997b1e01d8b82fc9c3dbda232
jesperingels <jesper.ingels@youaredigital.nl>
2018-05-28 11:52:59 +0200

    Delete button 2nd attempt

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
  * Created by jesperingels on 16-05-18.
  */
 
+let inputValue = document.getElementById("userInput").value;
+
 function name (klant){
 
     document.getElementById("name").textContent = klant;
 
 }
 
-name("Hilko");
+name(inputValue);
