3de63703f4abf9bbdfe40f056f028dc567581e10
jesperingels <jesper.ingels@youaredigital.nl>
2018-06-07 10:37:44 +0200

    Output database data on index.php

--- index.php
+++ index.php
@@ -5,21 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>You Are Digital</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <?php include_once 'includes/conn.php'; ?>
 </head> <!-- head end -->
 
 <body>
 
     <main>
 
-        <h1>Welkom <span id="var_name"></span></h1>
+        <h1>Welkom
+            <span>
+                <?php
+                    $sql = "SELECT  naam, tijdstip_van, tijdstip_tot FROM scherm WHERE id = 2;";
+                    $result = mysqli_query($conn, $sql);
+                    $person = mysqli_fetch_assoc($result);
+                    echo $person['naam'];
+                ?>
+            </span>
+        </h1>
         <aside></aside>
         <img class="logo" src="images/youaredigital.svg"/>
         <div class="text-wrapper">
 
             <h3>vergaderruimte</h3>
             <ul>
-                <li class="from">11:00</li>
-                <li class="until">12:30</li>
+                <li class="from"> <?php echo $person['tijdstip_van']?> </li>
+                <li class="until"> <?php echo $person['tijdstip_tot']?> </li>
             </ul>
 
         </div> <!-- text-wrapper end -->
