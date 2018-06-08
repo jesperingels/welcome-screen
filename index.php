e557b56fb4356b4429673077348fe1712168431a
jesperingels <jesper.ingels@youaredigital.nl>
2018-06-08 16:41:04 +0200

    Update comments

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Screen</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    //Include connection to the database
     <?php include_once 'includes/conn.php'; ?>
 </head> <!-- head end -->
 
@@ -15,40 +16,30 @@
         <h1>Welkom
             <span>
                 <?php
-                    //Store current date in Variable
+
+                    //Store current Date in Variable
                     $currentDate = date("Y/m/d");
                     //Store current Time in Variable
                     $currentTime = date("H:i:s");
 
-                    //Select data from database
-                    $sql = "SELECT  naam, datum, tijdstip_van, tijdstip_tot FROM scherm WHERE id=5;";
 
-
-                    //$sql = sprintf("SELECT * FROM `scherm` WHERE '%s' >= `tijdstip_van` and '%s' <= `tijdstip_tot` limit 1", $currentTime, $currentTime);
+                    //Set SQL statement
                     $sql ="SELECT * FROM `scherm` WHERE '$currentTime' >= `tijdstip_van` and '$currentTime' <= `tijdstip_tot` and `datum` = '$currentDate' limit 1;";
 
 
-                    //Send the sql to the database
+                    //Send the sql statement to the database
                     $result = mysqli_query($conn, $sql);
                     //Fetch the data from the database
                     $package = mysqli_fetch_assoc($result);
 
-
+                    //If $package contains data, show 'naam'
                     if (!empty($package)) {
                         echo $package['naam'];
-
+                    //Else show {Klantnaam}
                     } else {
                         echo '{Klantnaam}' . "<br>";
                         echo $currentDate;
                     }
-
-                    //Show data from column 'naam'
-//                    if($currentTime > $package['tijdstip_van'] && $currentTime < $package['tijdstip_tot']){
-//                    }
-//                        echo $package['naam']. "<br>";
-                   // echo "Current Time:" . $currentTime . "<br>";
-                   // echo "Database Time:" . $package['tijdstip_van'];
-
 
                 ?>
             </span>
